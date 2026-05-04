4a2405f981ace621523f1f278113f287a7f2a057
Jaeung Jang <Jangjaeung5@gmail.com>
2026-05-04 19:12:47 +0900

    Add admin unlimited label for hsbhyj0127

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -132,6 +132,7 @@ function normalizeSearchText(value = '') {
 
 const PROJECT_PREVIEW_LIMIT = 5
 const ADMIN_UNLIMITED_EMAILS = new Set([
+  'hsbhyj0127@gmail.com',
   'lab.dotory@gmail.com',
   'seojy1932@naver.com',
   'lgye322@gmail.com',
